f05b485ae830f8d5d58a0c4612903a7c78812c21
Jiang Guo <guolake@gmail.com>
2019-09-21 15:53:31 +0800

    refactor: reformat the code

--- src/components/ArticleList/ArticleList.tsx
+++ src/components/ArticleList/ArticleList.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import {getAllArticles, Response} from '../../api/api'
+import {getAllArticles} from '../../api/api'
 import ArticleItem from "../ArticleItem/ArticleItem"
 
 export interface Article {
@@ -17,11 +17,9 @@ interface IArticleListState {
 class ArticleList extends React.Component<{}, IArticleListState> {
     readonly state = {articles: Array<Article>()}
 
-    componentDidMount(): void {
-        getAllArticles().then((rep: Response<Article>) => {
-            console.log(rep)
-            this.setState({articles: rep.data as Array<Article>})
-        })
+    async componentDidMount(): Promise<void> {
+        let rep: Article[] = await getAllArticles() as Article[]
+        this.setState({articles: rep})
     }
 
     render() {
